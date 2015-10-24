40ffcd209014501c40b55434e7d69c89bc9ce9b5
Tomas Trajan <tomas.trajan@gmail.com>
2015-10-24 17:30:01 +0200

    fix(config): fix retrieving of dev and environment configuration

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 var dev = {};
 try {
-    dev = require('dev');
+    dev = require('./dev');
 } catch (err) {
     console.log('DEV config not found using process environment variables')
 }
 
 exports.PG_CONNECTION_STRING = process.env.PG_CONNECTION_STRING || dev.PG_CONNECTION_STRING;
 exports.AUTH_0_SECRET = process.env.AUTH_0_SECRET || dev.AUTH_0_SECRET;
+exports.AUTH_0_AUDIENCE = process.env.AUTH_0_AUDIENCE || dev.AUTH_0_AUDIENCE;
